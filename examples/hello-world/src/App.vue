c7e1969a7107407f5fc179642800876bac2ddf3d
Scott Bedard <scottbedard90@gmail.com>
2020-04-08 21:12:13 -0700

    begin scaffolding examples

---
+++ examples/hello-world/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Renderer>
+      <Scene />
+    </Renderer>
+  </div>
+</template>
+
+<script>
+import { Renderer, Scene } from '../../../dist/index.esm';
+
+export default {
+  components: {
+    Renderer,
+    Scene
+  },
+  name: 'App',
+}
+</script>
